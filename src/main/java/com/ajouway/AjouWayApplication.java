1c671a181ae7a360741507a8b0a759efd527dd2a
hyunzzii <yhynym@gmail.com>
2025-04-02 20:24:12 +0900

    init: 프로젝트 시작

---
+++ src/main/java/com/ajouway/AjouWayApplication.java
@@ -0,0 +1,13 @@
+package com.ajouway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjouWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajouway/AjouWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajouway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjouWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
