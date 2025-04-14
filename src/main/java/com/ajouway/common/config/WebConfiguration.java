f4964b9e5339df8a1d46d3b40be3e4ef69401c13
hyunzzii <yhynym@gmail.com>
2025-04-14 11:04:37 +0900

    feat: 프로젝트 기본 설정(Swagger, geojson, 에러) 추가

---
+++ src/main/java/com/ajouway/common/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.ajouway.common.config;
+
+import com.ajouway.common.converter.StringToEnumConverter;
+import com.ajouway.domain.enums.AmenityInfoType;
+import com.fasterxml.jackson.databind.util.Converter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public Converter<String, AmenityInfoType> amenityInfoTypeConverter(){
+        return new StringToEnumConverter<>(AmenityInfoType.class);
+    }
+}
