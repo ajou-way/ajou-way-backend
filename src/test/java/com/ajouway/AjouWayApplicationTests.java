22e7fe18f2dd3e72470bb1bb74f3c050c0be3217
hyunzzii <yhynym@gmail.com>
2025-04-14 18:54:20 +0900

    refactor: test active profile 설정 추가

--- src/test/java/com/ajouway/AjouWayApplicationTests.java
+++ src/test/java/com/ajouway/AjouWayApplicationTests.java
@@ -2,8 +2,10 @@ package com.ajouway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
+@ActiveProfiles("h2-test")
 @SpringBootTest
 class AjouWayApplicationTests {
 
