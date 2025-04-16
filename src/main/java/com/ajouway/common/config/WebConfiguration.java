75b4500f5027b439493de3c0ceef07ebd3d513b8
hyunzzii <yhynym@gmail.com>
2025-04-17 04:13:41 +0900

    cicd: cors설정

--- src/main/java/com/ajouway/common/config/WebConfiguration.java
+++ src/main/java/com/ajouway/common/config/WebConfiguration.java
@@ -5,6 +5,7 @@ import com.ajouway.domain.enums.AmenityInfoType;
 import com.fasterxml.jackson.databind.util.Converter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,14 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Bean
     public Converter<String, AmenityInfoType> amenityInfoTypeConverter(){
         return new StringToEnumConverter<>(AmenityInfoType.class);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
