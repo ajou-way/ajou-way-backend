6f43c3cd2f99cce753be47667ec6e725de4f074d
hyunzzii <yhynym@gmail.com>
2025-04-28 21:24:13 +0900

    fix: cors 문제 해결

--- src/main/java/com/ajouway/common/config/WebConfiguration.java
+++ src/main/java/com/ajouway/common/config/WebConfiguration.java
@@ -19,9 +19,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(
-                        "http://localhost:5173"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
