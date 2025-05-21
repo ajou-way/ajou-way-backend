2a3cd55435711e1936c543d669bf3e223b14af72
hyunzzii <yhynym@gmail.com>
2025-05-21 20:41:06 +0900

    astar 알고리즘 다익스트라로 변경

--- src/main/java/com/ajouway/storage/repository/route/EntranceRepository.java
+++ src/main/java/com/ajouway/storage/repository/route/EntranceRepository.java
@@ -14,25 +14,25 @@ public class EntranceRepository {
 
     public List<Long> findEntranceNodeIdsNearBuilding(Long osmId) {
         String sql = """
-                    SELECT DISTINCT ON (p.way)
-                      e.source AS node_id
-                    FROM planet_osm_point p
-                    JOIN LATERAL (
-                        SELECT e.source,
-                               ST_Distance(
-                                   ST_Transform(p.way, 3857),
-                                   e.geom
-                               ) AS dist
-                        FROM edges e
-                        ORDER BY dist
-                        LIMIT 1
-                    ) e ON TRUE
-                    WHERE p.tags -> 'entrance' IS NOT NULL
-                      AND ST_DWithin(
-                        ST_Transform(p.way, 3857),
-                        ST_Transform((SELECT way FROM planet_osm_polygon WHERE osm_id = ?), 3857),
-                        5
-                    )
+                    SELECT DISTINCT ON (p.osm_id)
+                       w_dist.source AS node_id
+                     FROM planet_osm_point p
+                     JOIN LATERAL (
+                         SELECT w.source,
+                                ST_Distance(
+                                    ST_Transform(p.way, 3857),
+                                    w.geom
+                                ) AS dist
+                         FROM ways w
+                         ORDER BY dist
+                         LIMIT 1
+                     ) w_dist ON TRUE
+                     WHERE p.tags -> 'entrance' IS NOT NULL
+                       AND ST_DWithin(
+                         ST_Transform(p.way, 3857),
+                         ST_Transform((SELECT way FROM planet_osm_polygon WHERE osm_id = ?), 3857),
+                         5
+                     );
                 """;
 
         return jdbcTemplate.query(sql, (rs, rowNum) -> rs.getLong("node_id"), osmId);
