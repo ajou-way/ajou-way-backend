b5228bb97647dc67318fb8b4021f19673dca6a1d
hyunzzii <yhynym@gmail.com>
2025-06-03 20:15:05 +0900

    refactor: jwt filter 수정 및 권환 확인 추가

--- src/main/java/com/ajouway/dto/auth/SocialLoginRequest.java
+++ src/main/java/com/ajouway/dto/auth/SocialLoginRequest.java
@@ -1,9 +1,14 @@
 package com.ajouway.dto.auth;
 
 import com.ajouway.domain.enums.AuthProvider;
+import jakarta.validation.constraints.NotBlank;
+import lombok.NonNull;
 
 public record SocialLoginRequest (
+        @NonNull
         AuthProvider provider,
+
+        @NotBlank
         String accessToken
 ){
 }
