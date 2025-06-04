843b5be4bbd5fa167dbaf990f9e342bb4e9057f7
hyunzzii <yhynym@gmail.com>
2025-06-04 22:00:24 +0900

    feat: 관리자 마커 수정 및 조회 기능 개발

--- src/main/java/com/ajouway/infra/persistence/AdminMarkerRepositoryImpl.java
+++ src/main/java/com/ajouway/infra/persistence/AdminMarkerRepositoryImpl.java
@@ -1,10 +1,15 @@
 package com.ajouway.infra.persistence;
 
+import com.ajouway.common.exception.CustomException;
+import com.ajouway.common.exception.CustomExceptionInfo;
 import com.ajouway.domain.repository.AdminMarkerRepository;
 import com.ajouway.storage.entity.map.AdminMarkerEntity;
 import com.ajouway.storage.repository.map.AdminMarkerJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,12 +17,23 @@ public class AdminMarkerRepositoryImpl implements AdminMarkerRepository {
     private final AdminMarkerJpaRepository adminMarkerJpaRepository;
 
     @Override
-    public AdminMarkerEntity save(AdminMarkerEntity adminMarkerEntity) {
+    public AdminMarkerEntity save(final AdminMarkerEntity adminMarkerEntity) {
         return adminMarkerJpaRepository.save(adminMarkerEntity);
     }
 
     @Override
-    public void deleteById(Long id) {
+    public void deleteById(final Long id) {
         adminMarkerJpaRepository.deleteById(id);
+    }
+
+    @Override
+    public AdminMarkerEntity getById(final Long id) {
+        return adminMarkerJpaRepository.findById(id)
+                .orElseThrow(() -> new CustomException(CustomExceptionInfo.NOT_FOUND_MARKER));
+    }
+
+    @Override
+    public List<AdminMarkerEntity> findAllByDeadlineAfter(final LocalDateTime now) {
+        return adminMarkerJpaRepository.findAllByDeadlineAfter(now);
     }
 }
--- src/main/java/com/ajouway/storage/repository/map/AdminMarkerJpaRepository.java
+++ src/main/java/com/ajouway/storage/repository/map/AdminMarkerJpaRepository.java
@@ -3,5 +3,9 @@ package com.ajouway.storage.repository.map;
 import com.ajouway.storage.entity.map.AdminMarkerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AdminMarkerJpaRepository extends JpaRepository<AdminMarkerEntity, Long> {
+    List<AdminMarkerEntity> findAllByDeadlineAfter(LocalDateTime now);
 }
